99dcf4cc3121bcb67abcf113e5392124aa74f033
Indra González Cabrera <Indra@10.225.173.197>
2016-03-02 19:13:20 +0000

    Faltan pruebas y NO FUNCIONA

--- src/ramificacionyacotacion/Arbol.java
+++ src/ramificacionyacotacion/Arbol.java
@@ -15,4 +15,18 @@ public class Arbol {
     public void insertaNodo(Nodo nodo){
         estructura.add(nodo);
     }
+    
+    public void printArbol(){
+        System.out.println("El árbol tiene los siguientes nodos: ");
+        for (int i = 0; i < estructura.size(); i++) {
+            Nodo nodo = estructura.get(i);
+            System.out.println("Nodo " + nodo.vertice.id + " con estapas: [" + 
+                    nodo.vertice.etapas[0] + "," + nodo.vertice.etapas[1] + 
+                    "] ");
+            /*for (int j = 0; j < nodo.pesoHijos.size(); j++) {
+                System.out.println();
+                
+            }*/
+        }
+    }
 }
